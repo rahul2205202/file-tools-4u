c4ee7a0f92e89f23cdfdb38a4eae13ae57a1eeaf
Rahul <rahulbodkhe2205@gmail.com>
2025-09-12 20:40:16 +0530

    sitemap updated

--- src/app/sitemap.jsx
+++ src/app/sitemap.jsx
@@ -12,6 +12,7 @@ export default function sitemap() {
     '/jpeg-to-png',
     '/png-to-jpeg',
     '/photo-restorer',
+    '/ai-figurine-creator',
     '/webp-to-png',
     '/webp-converter',
     '/contact-us',
