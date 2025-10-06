6804c134cb07697e43373854d975f13bf3943816
Rahul <rahulbodkhe2205@gmail.com>
2025-10-06 10:42:57 +0530

    docker fix

--- next.config.js
+++ next.config.js
@@ -2,22 +2,21 @@
 const nextConfig = {
   experimental: {
     serverActions: {
-      bodySizeLimit: '50mb', // Allow uploads up to 50MB
+      bodySizeLimit: '50mb',
     },
   },
-  // --- NEW: Redirects Configuration ---
   async redirects() {
     return [
-      // NEW: Rule to redirect www to non-www
+      
       {
-        source: '/:path*', // This matches all paths
+        source: '/:path*',
         has: [
           {
             type: 'host',
             value: 'www.filetools4u.com',
           },
         ],
-        destination: 'https://filetools4u.com/:path*', // Redirect to the non-www version
+        destination: 'https://filetools4u.com/:path*',
         permanent: true,
       },
     ];
