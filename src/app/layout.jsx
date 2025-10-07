a23ed1491922ac8dba7e48c91c5ce5bee66999b5
Rahul <rahulbodkhe2205@gmail.com>
2025-10-07 14:41:19 +0530

    hydration issue fixed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
-'use client'; // Client Component for hooks like usePathname
+'use client';
 
-import './globals.css'; 
+import './globals.css';
 import Header from './components/shared/Header';
 import Footer from './components/shared/Footer';
 import Providers from './components/shared/Providers';
@@ -9,7 +9,6 @@ import { usePathname } from 'next/navigation';
 import { useEffect } from 'react';
 import Script from 'next/script';
 
-// Your GA4 Measurement ID
 const GA_ID = 'G-90ZGVP1H92';
 
 export default function RootLayout({ children }) {
@@ -36,40 +35,34 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <head>=
-        <Script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-        />
-        <Script id="google-analytics-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_ID}', { send_page_view: false });
-          `}
-        </Script>
-      </head>
-
       <body>
         <Providers>
+          <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}', { send_page_view: false });
+            `}
+          </Script>
+
           <div className="flex flex-col min-h-screen bg-background">
             <Header />
             <main className="flex-grow py-1">
               {showSidebar ? (
                 <div className="flex lg:pl-30 lg:pr-30 py-5 mx-auto px-4">
                   <div className="flex flex-col lg:flex-row gap-5">
-                    <div className="flex-1">
-                      {children}
-                    </div>
+                    <div className="flex-1">{children}</div>
                     <Sidebar />
                   </div>
                 </div>
               ) : (
                 <div className="flex lg:pl-30 lg:pr-30 py-5 mx-auto px-4">
-                  <div className="flex justify-center">
-                    {children}
-                  </div>
+                  <div className="flex justify-center">{children}</div>
                 </div>
               )}
             </main>
